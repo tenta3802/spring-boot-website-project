e379a5b7b6db714d654610e249f348ee81999973
tenta <eunoia3802@gmail.com>
2023-02-12 23:52:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/ubstore/ubstore/UbstoreApplication.java
@@ -0,0 +1,13 @@
+package com.ubstore.ubstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbstoreApplication.class, args);
+	}
+
+}
